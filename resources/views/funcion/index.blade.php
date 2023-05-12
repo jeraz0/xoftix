be435b29895bb0a766be6419d6b6ca670fb347d8
jeraz0 <juanpa.erazo@umariana.edu.co>
2023-05-12 02:31:14 -0500

    haciendo comentarios

--- resources/views/funcion/index.blade.php
+++ resources/views/funcion/index.blade.php
@@ -1,18 +1,25 @@
 @extends('../templates.template')
 @section('titulo','Sistema de Seguimiento - Coordinaciones')
 @section('content')
+{{--Esquema principal para ingreso de función--}}
 <div>
     <div class="btn-group">
+        {{--Botón para agregar paréntesis--}}
         <button id="parentesis" class="btn btn-success fa-solid fa-check"></button>
+        {{--Botón para eliminar función--}}
         <button id="eliminar" class="btn btn-danger fa-solid fa-xmark"></button>
+        {{--Botón para buscar funciones e información--}}
         <button type="button" class="btn btn-primary fa-solid fa-f" data-bs-toggle="modal" data-bs-target="#exampleModal">
         </button>        
+        {{--Campo para mostrar resultados de la función--}}
         <label id="expresion" class="form-label"></label>
     </div>
     <div>
+        {{--Campo para ingresar función--}}
          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="entrada" name="coordinacion" type="text" placeholder="ingrese ecuación">
     </div>
 </div>
+{{--Ventana para visualizar lista de funciones--}}
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
@@ -24,12 +31,15 @@
             <div class="container-fluid">
                 <div class="row">
                   <div class="col-md-6">
+                    {{-- Barra de busqueda --}}
                     <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" name="" id="busqueda" placeholder="Search">
                     <div><br>
+                        {{--Lista de Funciones--}}
                         <select name="" class="form-control" multiple id="lista"></select>
                     </div>
                   </div>
                   <div class="col-md-6">
+                    {{--Variable que muestra la información de la función--}}
                     <label id="info"></label>
                   </div>
                 </div>
--- resources/views/templates/template.blade.php
+++ resources/views/templates/template.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    {{--CDN's de las extensiones utilizadas con css--}}
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -9,25 +10,37 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script></head>
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.1/themes/smoothness/jquery-ui.css">
     <body>
+    {{--CDN's complementarios para Java Script--}}
     @yield('content')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/1175464dc3.js" crossorigin="anonymous"></script>
+    {{--Script que importa la clase mathJs--}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/mathjs/7.5.1/math.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.min.js"></script>
+    {{--Script que controla el entorno web--}}
     <script>
+        // Variable de etrada de la función
         var entrada =  document.getElementById('entrada');
+        // Variable que muestra el resultado de la función
         var label = document.getElementById('expresion');
+        // Variable que permite la busqueda y filtro de funciones de la librería mathjs
         var busqueda = document.getElementById('busqueda');
+        // Variable que almacena la lista de funciones
         var lista = document.getElementById('lista');
+        // Variable que ejecuta el botón de paréntesis (verde)
         var parentesis = document.getElementById('parentesis');
+        // Variable que ejecuta el botón de eliminar la función (rojo)
         var eliminar = document.getElementById('eliminar');
+        // Variable que ejecuta el botón de buscar funciones (azul)
         var informacion = document.getElementById('info');
 
+        // Obteniendo las funciones de la libreria mathjs
         const funciones = Object.keys(math);
 
+        // Inicializando la lista de funciones con todas ellas
         funciones.forEach(function(funcion) {
             const nuevoElemento = document.createElement('option');
             nuevoElemento.value = funcion;
@@ -35,6 +48,7 @@
             lista.appendChild(nuevoElemento);
         });
 
+        // Filtrando las funciones según la entrada
         busqueda.addEventListener('input',function(){
             lista.length = 0;
             funciones.forEach(funcion => 
@@ -50,21 +64,17 @@
             }); 
         });
         
-        
+        // Mostrar la información de la función seleccionada
         lista.addEventListener('change',function(){
-            console.log(math.help(lista.value));
             if(math.help(lista.value)!="")
             {
                 informacion.textContent = math.help(lista.value);
             }
         });
 
-        console.log(math.help('sin'));
-
-        var valores = "";
+        // Aplicando una herramienta de sugerencias para llevar a cabo la función
         entrada.addEventListener('input',function() 
         {
-            valores = ""
             $('#entrada').autocomplete({
             source: funciones,
             });
@@ -73,9 +83,13 @@
                 label.textContent =entrada.value+' = '+math.evaluate(entrada.value);
             }
         });
+
+        // Aplicando la funcionalidad al botón de parentesis
         parentesis.addEventListener('click',function(){
             entrada.value+= '(  )';
         });
+
+        // Aplicando la funcionalidad al botón de eliminar función
         eliminar.addEventListener('click',function(){
             entrada.value = '';
             label.textContent = '';
